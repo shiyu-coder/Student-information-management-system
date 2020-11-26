d7085d3a8c5414e4bcdf5a8bc8dd7ae06d4b11d3
shiyu <3122067540@qq.com>
2020-11-26 23:31:27 +0800

    测试Qt的db组建的数据库连接功能

--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include<QtDebug>
+#include<QtSql>
+#include<QSqlDatabase>
+#include<QSqlQuery>
+#include<QSqlQuery>
+#include<QVariant>
+#include<QSqlError>
+#include<QString>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    QSqlDatabase db=QSqlDatabase::addDatabase("QODBC");
+    db.setDatabaseName("E_Chain_Database");
+    db.setUserName("sa");
+    db.setHostName("192.168.56.1");
+    db.setPassword("9638527410.s");
+    if(!db.open()){
+        qDebug()<<"fail to open!"<<endl<<db.lastError();
+    }else{
+        qDebug()<<"successful!"<<endl;
+        db.close();
+    }
     return a.exec();
 }
