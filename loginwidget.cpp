d4fc2ab928c68f50ca73b3f475a2adb38663d40f
shiyu <3122067540@qq.com>
2020-12-01 19:41:39 +0800

    登陆界面ui优化、管理员界面ui搭建（部分）

--- loginwidget.cpp
+++ loginwidget.cpp
@@ -1,6 +1,7 @@
 #include "loginwidget.h"
 #include "ui_loginwidget.h"
 #include<mainwindow.h>
+#include<adminwindow.h>
 
 LogInWidget::LogInWidget(QWidget *parent) :
     QWidget(parent),
@@ -23,8 +24,8 @@ void LogInWidget::on_ConfirmButton_clicked()
 
 void LogInWidget::on_toolButton_clicked()
 {
-    MainWindow *mainWindow=new MainWindow();
-    mainWindow->show();
+    AdminWindow *adminWindow=new AdminWindow();
+    adminWindow->show();
     this->close();
 
 }
