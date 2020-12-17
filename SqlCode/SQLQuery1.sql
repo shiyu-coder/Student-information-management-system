d13e6f66ec440e1d9783722d45b8e89b5f73edd7
shiyu-coder <3122067540@qq.com>
2020-12-17 10:29:39 +0800

    实现新建学生用户时的数据库处理操作

--- SqlCode/SQLQuery1.sql
+++ SqlCode/SQLQuery1.sql
@@ -7,12 +7,18 @@
 4. 承接的大创的项目，可以批准或者拒绝
 */
 
+CREATE ROLE Teacher_Tno
+
 CREATE VIEW TC_Tno /*e.g. TC_1*/
 AS 
 SELECT Tcourse.Cterm, CourseBasic.Cno, Cname, Wday, Cbegin, Cend
 FROM CourseBasic, CTime, Tcourse
 WHERE Tcourse.Tno='x' AND Tcourse.Cno=CourseBasic.Cno
 AND Tcourse.Cno=CTime.Cno AND Tcourse.Cterm=CTime.Cterm
+
+GRANT SELECT
+ON TC_Tno
+TO Teacher_Tno
 
 /*一个老师教授的一门课的具体学生信息，用于后续登录成绩*/
 CREATE VIEW TCS_Tno__Cno_Cterm 
@@ -21,11 +27,19 @@ SELECT Sno, Grade
 FROM Stu_Cour
 WHERE Stu_Cour.Cno='x' AND Stu_Cour.Cterm='y'
 
+GRANT SELECT, UPDATE(Grade)
+ON TCS_Tno__Cno_Cterm 
+TO Teacher_Tno
+
 CREATE VIEW T2A_Tno
 AS
 SELECT Rcontent, Response
 FROM T2A
 WHERE T2A.Tno='x'
+
+GRANT SELECT, UPDATE(Rcontent),INSERT/*?*/
+ON T2A_Tno
+TO Teacher_Tno
 
 CREATE VIEW TP_Tno
 AS
@@ -33,22 +47,64 @@ SELECT Sno, ProjectName, Reason, Response
 FROM ProjectAppli, ProjectLst
 WHERE ProjectLst.Tno='x' AND ProjectAppli.ProjectName=ProjectLst.ProgramName
 
-CREATE ROLE Teacher_Tno
-
-GRANT SELECT
-ON TC_Tno
-TO Teacher_Tno
-
-GRANT SELECT, UPDATE(Grade)
-ON TCS_Tno__Cno_Cterm 
-TO Teacher_Tno
-
-GRANT SELECT, UPDATE(Rcontent)
-ON T2A_Tno
-TO Teacher_Tno
-
 GRANT SELECT, UPDATE(Response)
 ON TP_Tno
-TO TP_Tno
+TO Teacher_Tno
 
 
+
+/*
+学生的视图
+1. 所有学生都有的，可以查看所有课程的信息
+2. 自己所有选课信息（退课要申请，这里不能退）
+3. 只能查看自己的成绩表
+4. 学生查看自己的基本信息
+5. 自己的一般申请
+6. 自己的奖学金申请
+7. 自己的大创申请
+*/
+
+CREATE ROLE Student_Sno
+
+
+create login 施宇 with password='123456', default_database=E_Chain;
+create user 施宇 for login 施宇 with default_schema=dbo
+
+CREATE VIEW Sno_Info_0000001 AS SELECT Sno , Sname, Ssex, Sdept, Sgrade FROM Student WHERE Sno='0000001'
+
+GRANT SELECT ON Sno_Info_0000001 TO 施宇
+/*学生查看自己所有选课*/
+
+
+CREATE VIEW Sno_Course_0000001 AS SELECT DISTINCT Stu_Cour.Cno, Stu_Cour.Cterm FROM Stu_Cour WHERE Stu_Cour.Sno='x'
+/*insert是选课的时候插入的*/
+GRANT SELECT, INSERT ON Sno_Course TO 施宇
+
+/*与这个视图一起可以查看这位同学的选课课表*/
+SELECT Sno_Course.Cno, Cname, Tname, Sno_Course.Cterm, Wday, Cbegin, Cend FROM Sno_Course, CourseBasic, CTime, Tcourse, Teacher WHERE Sno_Course.Cno=CourseBasic.Cno AND Sno_Course.Cno=Tcourse.Cno AND Sno_Course.Cterm=Tcourse.Cterm AND Sno_Course.Cno=CTime.Cno AND Sno_Course.Cterm=CTime.Cterm
+
+
+
+
+
+CREATE VIEW Sno_Grade AS SELECT DISTINCT Stu_Cour.Cno, Cname, Cchar, Ccredit, Grade,Cterm FROM Stu_Cour, CourseBasicWHERE Stu_Cour.Sno='x' AND Stu_Cour.Cno=CourseBasic.Cno 
+
+GRANT SELECT ON Sno_Grade TO Student_Sno
+
+
+
+CREATE VIEW Sno_S2A AS SELECT Rcontent, Response FROM S2A WHERE Sno='x'
+/*但是学生和老师不能写response，这个有点难顶*/
+GRANT SELECT, UPDATE(Rcontent),INSERT  ON Sno_S2A TO Student_Sno
+
+
+
+CREATE VIEW Sno_Pro AS SELECT ProjectName, Reason, Response FROM ProjectAppli WHERE Sno='x'
+ /*但是学生不能写response?*/
+GRANT SELECT, UPDATE(ProjectName, Reason),INSERT ON Sno_Pro TO Student_Sno
+
+
+
+CREATE VIEW Sno_Scho AS SELECT Scholarship, Reason, Response FROM ScholarAppli WHERE Sno='x'
+/*但是学生不能写response?*/ 
+GRANT SELECT, UPDATE(Scholarship, Reason),INSERT ON Sno_Scho TO Student_Sno
--- addstuwidget.cpp
+++ addstuwidget.cpp
@@ -67,8 +67,39 @@ void AddStuWidget::on_ConfirmButton_clicked()
     qDebug()<<"insert into Student values('"+info.at(0)+"','"+info.at(1)+"','"+info.at(2)+"','"+info.at(3)+"','"+info.at(4)+"')";
     query.exec("insert into Student values('"+info.at(0)+"','"+info.at(1)+"','"+info.at(2)+"','"+info.at(3)+"','"+info.at(4)+"')");
     if(query.lastError().type()==QSqlError::NoError){
-        QMessageBox::information(this,"新增学生","新增学生成功！");
-        this->close();
+        QStringList sqls;
+        sqls<<"create login "+info.at(1)+" with password='123456', default_database=E_Chain";
+        sqls<<"create user "+info.at(1)+" for login "+info.at(1)+" with default_schema=dbo";
+        sqls<<"CREATE VIEW Sno_Info_"+info.at(0)+" AS SELECT Sno , Sname, Ssex, Sdept, Sgrade FROM Student WHERE Sno='"+info.at(0)+"'";
+        sqls<<"GRANT SELECT ON Sno_Info_"+info.at(0)+" TO "+info.at(1)+"";
+        sqls<<"CREATE VIEW Sno_Course_"+info.at(0)+" AS SELECT DISTINCT Stu_Cour.Cno, Stu_Cour.Cterm FROM Stu_Cour WHERE Stu_Cour.Sno='"+info.at(0)+"'";
+        sqls<<"GRANT SELECT, INSERT ON Sno_Course_"+info.at(0)+" TO "+info.at(1)+"";
+        sqls<<"SELECT Sno_Course_"+info.at(0)+".Cno, Cname, Tname, Sno_Course_"+info.at(0)+".Cterm, Wday, Cbegin, Cend FROM Sno_Course_"+info.at(0)+", CourseBasic, CTime, Tcourse, Teacher WHERE Sno_Course_"+info.at(0)+".Cno=CourseBasic.Cno AND Sno_Course_"+info.at(0)+".Cno=Tcourse.Cno AND Sno_Course_"+info.at(0)+".Cterm=Tcourse.Cterm AND Sno_Course_"+info.at(0)+".Cno=CTime.Cno AND Sno_Course_"+info.at(0)+".Cterm=CTime.Cterm";
+        sqls<<"CREATE VIEW Sno_Grade_"+info.at(0)+" AS SELECT DISTINCT Stu_Cour.Cno, Cname, Cchar, Ccredit, Grade,Cterm FROM Stu_Cour, CourseBasic WHERE Stu_Cour.Sno='"+info.at(0)+"' AND Stu_Cour.Cno=CourseBasic.Cno ";
+        sqls<<"GRANT SELECT ON Sno_Grade_"+info.at(0)+" TO "+info.at(1);
+        sqls<<"CREATE VIEW Sno_S2A_"+info.at(0)+" AS SELECT Rcontent, Response FROM S2A WHERE Sno='"+info.at(0)+"'";
+        sqls<<"GRANT SELECT, UPDATE(Rcontent),INSERT  ON Sno_S2A_"+info.at(0)+" TO "+info.at(1);
+        sqls<<"CREATE VIEW Sno_Pro_"+info.at(0)+" AS SELECT ProjectName, Reason, Response FROM ProjectAppli WHERE Sno='"+info.at(0)+"'";
+        sqls<<"GRANT SELECT, UPDATE(ProjectName, Reason),INSERT ON Sno_Pro_"+info.at(0)+" TO "+info.at(1);
+        sqls<<"CREATE VIEW Sno_Scho_"+info.at(0)+" AS SELECT Scholarship, Reason, Response FROM ScholarAppli WHERE Sno='"+info.at(0)+"'";
+        sqls<<"GRANT SELECT, UPDATE(Scholarship, Reason),INSERT ON Sno_Scho_"+info.at(0)+" TO "+info.at(1);
+
+        QString res;
+        for(int i=0;i<sqls.size();i++){
+            query.exec(sqls.at(i));
+            if(query.lastError().type()==QSqlError::NoError){
+
+            }else{
+                res+=query.lastError().text();
+            }
+        }
+        if(res==""){
+            QMessageBox::information(this,"新增学生","新增学生成功！");
+            this->close();
+        }else{
+            QMessageBox::warning(this,"创建学生用户及视图错误",res);
+        }
+
     }else{
         QMessageBox::warning(this,"新增学生错误",query.lastError().text());
     }
